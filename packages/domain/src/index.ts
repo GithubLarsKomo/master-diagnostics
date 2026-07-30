28c6f5698c7298ef737ddc6175f70a88d47c88d9
GithubLarsKomo <76850395+GithubLarsKomo@users.noreply.github.com>
2026-07-30 08:18:55 +0200

    Export test termination contract

--- packages/domain/src/index.ts
+++ packages/domain/src/index.ts
@@ -3,5 +3,6 @@ export * from './roles';
 export * from './types';
 export * from './test-planning';
 export * from './test-safety';
+export * from './test-termination';
 
 export * from './authorization';
