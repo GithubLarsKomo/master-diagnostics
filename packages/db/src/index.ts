8514b8ee2e01fcaa0eaeb8dc06f65d3bb2b0ce00
GithubLarsKomo <76850395+GithubLarsKomo@users.noreply.github.com>
2026-08-05 12:31:16 +0200

    feat(epic10): export pseudonymization readiness

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -11,6 +11,7 @@ export * from './services/consents';
 export * from './services/guardians';
 export * from './services/deletion-requests';
 export * from './services/retention';
+export * from './services/pseudonymization-readiness';
 export * from './services/test-plans';
 export * from './services/test-safety';
 export * from './services/test-lifecycle';
