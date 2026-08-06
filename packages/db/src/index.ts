b8713d3ee74e39590ff1d25d6918300a937241d8
GithubLarsKomo <76850395+GithubLarsKomo@users.noreply.github.com>
2026-08-06 18:46:21 +0200

    feat(epic12): export restore privacy ledger

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -17,6 +17,7 @@ export * from './services/data-subject-delivery-approval';
 export * from './services/data-subject-reviewed-delivery';
 export * from './services/data-subject-delivery-packages';
 export * from './services/data-subject-delivery-cleanup';
+export * from './services/restore-privacy-ledger';
 export * from './services/club-bootstrap';
 export * from './services/factory-protocol-templates';
 export * from './services/protocol-templates';
